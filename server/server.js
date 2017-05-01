858c3a8a07fff2892ab7fac74a4914ee652f1002
Sam Sung <anan1411@gmail.com>
2017-05-01 00:15:37 -0700

    refactor directory structure

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app     = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/../client'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(msg) {
+  	console.log('someone said: ', msg);
   	io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
