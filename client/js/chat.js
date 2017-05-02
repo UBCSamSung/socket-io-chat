0f28e6608b91cb5e5b58b437231c09ab7e90683d
Sam Sung <anan1411@gmail.com>
2017-05-02 15:04:17 -0700

    Added username functionality

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,9 +1,18 @@
-var playerName
 var socket = io();
+
 $(document).ready(function(){
-  $("form").submit(function(){
-    socket.emit('chat message', $('#message_input').val());
-    $("#message_input").val("");
+  $("#name_form").submit(function(){
+    var form = $("#name_input");
+    var name = $("#name_input").val();
+    $("#welcome").hide();
+    $("#chat").show();
+    socket.emit("new user", $("#name_input").val());
+    return false;
+  });
+  $("#chat_form").submit(function(){
+    socket.emit('chat message', $('#chat_input').val());
+    $("#chat_input").val("");
+    $("#chat_input").focus();
     return false;
   });
   socket.on('chat message', function(msg){
@@ -11,3 +20,5 @@ $(document).ready(function(){
     window.scrollTo(0, document.body.scrollHeight);
   });
 });
+
+function usernamePopup() {}
--- server/server.js
+++ server/server.js
@@ -2,21 +2,31 @@ var express = require('express');
 var app     = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var HashMap = require('hashmap');
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../client'));
 
+var userMap = new HashMap();
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg) {
-  	console.log('a user says: ', msg);
-  	io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+	console.log('a user connected');
+	socket.on('chat message', function(msg) {
+		var full_msg = userMap.get(socket.id) + ": " + msg;
+		console.log(full_msg);
+		io.emit('chat message', full_msg);
+	});
+	socket.on('new user', function(name) {
+		userMap.set(socket.id, name);
+		var full_msg = userMap.get(socket.id) + " has entered the chat.";
+		console.log(full_msg);
+		io.emit('chat message', full_msg);
+	});
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
