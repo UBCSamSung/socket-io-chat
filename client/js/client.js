659a472cee809ff435a4c84077d86c651dc8f2f3
Sam Sung <anan1411@gmail.com>
2017-05-03 15:29:43 -0700

    Remove duplicate chat message socket listener

--- client/js/client.js
+++ client/js/client.js
@@ -5,11 +5,7 @@ $(document).ready(function(){
   $("#name_form").submit(function(){
     return false;
   });
-
-  socket.on('chat message', function(msg){
-    $("#messages").append($('<li class="list-group-item">').text(msg));
-    window.scrollTo(0, document.body.scrollHeight);
-  });  
+ 
   socket.on('disconnect', function () {
       socket.close();
       alert("disconnected");
